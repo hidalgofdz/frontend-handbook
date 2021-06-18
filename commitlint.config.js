26626d2e3ab2e385ac6dff13b3c00016d41c7d29
Hidalgo Fernandez <hidalgofdz@gmail.com>
2021-06-18 17:01:49 -0500

    config: add storybook

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+const config = require("@commitlint/config-conventional");
+const types = config.rules["type-enum"][2];
+
+/*
+ * config: should be used when you add or change project configurations. For example, adding a linter, change
+ * code formatter, etc.
+ * */
+config.rules["type-enum"][2] = ["config"].concat(types);
+
+module.exports = config;
